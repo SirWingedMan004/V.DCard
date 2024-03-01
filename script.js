16a2240ed2bde16b57413b832d20e3aea47f329f
SirWingedMan004 <160008979+SirWingedMan004@users.noreply.github.com>
2024-03-01 20:58:57 +1300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('rsvpButton').addEventListener('click', function() {
+    alert('Thank you for your interest! Please send your RSVP to communitygreeninitiative@example.com');
+});
